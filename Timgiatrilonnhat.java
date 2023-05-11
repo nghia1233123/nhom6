5138eafb7412b7093297a734e935a5c6dd8c044f
nghia1233123 <133145799+nghia1233123@users.noreply.github.com>
2023-05-11 23:50:00 +0700

    Update Timgiatrilonnhat.java

--- Timgiatrilonnhat.java
+++ Timgiatrilonnhat.java
@@ -5,26 +5,11 @@ public class Timgiatrilonnhat {
    
     Scanner input  = new Scanner(System.in);
     System.out.println("Nhap a: " );
-     int a = input.nextInt() ; 
-    System.out.println("Nhap b: ");
-       int b = input.nextInt() ;
+     int a = input.nextInt() ;
     System.out.println("Nhap c: ");
        int c = input.nextInt() ; 
     System.out.println("Nhap d: " );
        int d = input.nextInt() ;
-    if( a>=b&&a>=c&&a>=d)
-    {
-            System.out.println(a+"\t a lon nhat ");
-    }
-    else if(b>=a && b>=c &&  b>=d) {
-            System.out.println(b+ "\t b lon nhat");
-    } else if(c>=a && c>=b &&  c>=d) {
-            System.out.println(b+ "\tb lon nhat");
-    }else
-        System.out.println(d+ "\t d lon nhat");
-        
-   
-    
-    }
+
     
 }
