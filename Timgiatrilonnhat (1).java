85b598c25dafd889d5b8375fc56fea77b56167f9
nhan137 <133251547+nhan137@users.noreply.github.com>
2023-05-12 00:20:20 +0700

    nhan137
    thêm dòng code

---
+++ Timgiatrilonnhat (1).java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner ; 
+public class Timgiatrilonnhat {
+ public static void main(String[] args) {
+   
+    Scanner input  = new Scanner(System.in);
+    System.out.println("Nhap a: " );
+     int a = input.nextInt() ; 
+    System.out.println("Nhap b: ");
+       int b = input.nextInt() ;
+    System.out.println("Nhap c: ");
+       int c = input.nextInt() ; 
+    System.out.println("Nhap d: " );
+       int d = input.nextInt() ;
+//Thêm dòng doãn nhân c?p nh?t
+else if(c>=a && c>=b &&  c>=d) {
+            System.out.println(b+ "\tb lon nhat");
+    }else
+        System.out.println(d+ "\t d lon nhat");
+        
+   
+    
+    
+}
+// truongtattrung cap nhap
