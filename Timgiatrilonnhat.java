73bc89d2ade94c6ca62b384e9bdee1dfc96a7e02
nghia1233123 <133145799+nghia1233123@users.noreply.github.com>
2023-05-11 23:36:39 +0700

    Add files via upload

---
+++ Timgiatrilonnhat.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner ; 
+public class Timgiatrilonnhat {
+ public static void main(String[] args) {
+   
+    Scanner input  = new Scanner(System.in);
+    System.out.println("Nhap a: " );
+     int a = input.nextInt() ; 
+    System.out.println("Nhap b: ");
+       int b = input.nextInt() ;
+    System.out.println("Nhap c: ");
+       int c = input.nextInt() ; 
+    System.out.println("Nhap d: " );
+       int d = input.nextInt() ;
+    if( a>=b&&a>=c&&a>=d)
+    {
+            System.out.println(a+"\t a lon nhat ");
+    }
+    else if(b>=a && b>=c &&  b>=d) {
+            System.out.println(b+ "\t b lon nhat");
+    } else if(c>=a && c>=b &&  c>=d) {
+            System.out.println(b+ "\tb lon nhat");
+    }else
+        System.out.println(d+ "\t d lon nhat");
+        
+   
+    
+    }
+    
+}
