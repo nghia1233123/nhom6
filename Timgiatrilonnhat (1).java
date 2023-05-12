f0e42729ec02bda05825224acd078a2bf465674d
nhuquynh2003ht <133378940+nhuquynh2003ht@users.noreply.github.com>
2023-05-12 22:11:12 +0700

    Update Timgiatrilonnhat (1).java

--- Timgiatrilonnhat (1).java
+++ Timgiatrilonnhat (1).java
@@ -1,34 +1,29 @@
+// truongtattrung cap nhap
+import java.util.Scanner;
 
-import java.util.Scanner ; 
 public class Timgiatrilonnhat {
- public static void main(String[] args) {
-   
-    Scanner input  = new Scanner(System.in);
-    System.out.println("Nhap a: " );
-     int a = input.nextInt() ; 
-    System.out.println("Nhap b: ");
-       int b = input.nextInt() ;
-    System.out.println("Nhap c: ");
-       int c = input.nextInt() ; 
-    System.out.println("Nhap d: " );
-       int d = input.nextInt() ;
-//Thêm dòng doãn nhân c?p nh?t
-else if(c>=a && c>=b &&  c>=d) {
-            System.out.println(b+ "\tb lon nhat");
-    }else
-        System.out.println(d+ "\t d lon nhat");
-//Thêm dòng quang s? c?p nh?t
-else if(c>=a && c>=b &&  c>=d) {
-            System.out.println(b+ "\tb lon nhat");
-    }else
-        System.out.println(d+ "\t d lon nhat");
-        
-   
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap a: ");
+        int a = input.nextInt();
+        System.out.println("Nhap b: ");
+        int b = input.nextInt();
+        System.out.println("Nhap c: ");
+        int c = input.nextInt();
+        System.out.println("Nhap d: ");
+        int d = input.nextInt();
+      //ThÃªm dÃ²ng doÃ£n nhÃ¢n c?p nh?t
+        if (a >= b && a >= c && a >= d) {
+            System.out.println(a + "\t a lon nhat");
+        } else if (b >= a && b >= c && b >= d) {
+            System.out.println(b + "\t b lon nhat");
+         //ThÃªm dÃ²ng quang s? c?p nh?t
+        } else if (c >= a && c >= b && c >= d) {
+            System.out.println(c + "\t c lon nhat");
+        } else {
+            System.out.println(d + "\t d lon nhat");
+        }
+
     }
-        
-   
-    
-    
 }
 // truongtattrung cap nhap
